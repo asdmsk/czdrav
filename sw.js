09311fa8f6f841db092ebd81bb35f90eab758d0f
asdmsk <asdmsk1@gmail.com>
2026-02-24 19:40:59 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'czdrav-cache-v3';
+const CACHE_NAME = 'czdrav-cache-v4';
 const urlsToCache = [
   './',
   './index.html',
@@ -24,3 +24,4 @@ self.addEventListener('fetch', (event) => {
   );
 
 });
+
